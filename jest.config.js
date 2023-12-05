83a37dea5c866d3bdfac89e6cf15ee4df383df15
alex <57722812+alexwhitmore@users.noreply.github.com>
2023-12-04 22:33:58 -0700

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   testMatch: ['**/*.test.js'],
   setupFilesAfterEnv: ['./jest.setup.js'],
+  reporters: ["default", ["jest-junit", { outputDirectory: "test-results", outputName: "junit.xml" }]]
 }
